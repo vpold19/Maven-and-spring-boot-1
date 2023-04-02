225c1f46ab2f9dbb1768569e39c5ec99ce41982c
vadim <vadimpoldolin19@gmail.com>
2023-04-02 23:29:17 +0200

    final

---
+++ src/main/java/Service/CalculatorService.java
@@ -0,0 +1,19 @@
+package Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public float plus(float a, float b) {
+        return a + b;
+    }
+    public float minus(float a, float b) {
+        return a - b;
+    }
+    public float multiply(float a, float b) {
+        return a * b;
+    }
+    public float divide(float a, float b) {
+        return a / b;
+    }
+}
--- src/main/java/com/skypro/vadim/calculator/controller/ControllerCalculator.java
+++ src/main/java/com/skypro/vadim/calculator/controller/ControllerCalculator.java
@@ -1,19 +1,40 @@
 package com.skypro.vadim.calculator.controller;
 
+import Service.CalculatorService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerCalculator {
+private final CalculatorService calculatorService;
+
+    public ControllerCalculator(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
     @GetMapping("/calculator")
     public String helloToCalculator() {
         return "<h1> Добро пожаловать в калькулятор </h1>";
     }
 
     @GetMapping("/calculator/plus")
-    public String plus(@RequestParam("num1") float a, @RequestParam ("num2") float b ) {
-        return a + " + " + b + " + " + (a + b);
+    public String plus(@RequestParam("num1") float a, @RequestParam("num2") float b) {
+        return a + " + " + b + " = " + (a + b);
+    }
+
+    @GetMapping("/calculator/minus")
+    public String minus(@RequestParam("num1") float a, @RequestParam("num2") float b) {
+        return a + " - " + b + " = " + (a - b);
+    }
+
+    @GetMapping("/calculator/multiply")
+    public String multiply(@RequestParam("num1") float a, @RequestParam("num2") float b) {
+        return a + " * " + b + " = " + (a * b);
+    }
+
+    @GetMapping("/calculator/divide")
+    public String divide(@RequestParam("num1") int a, @RequestParam("num2") int b) {
+        return a + " / " + b + " = " + (a / b);
     }
 }
